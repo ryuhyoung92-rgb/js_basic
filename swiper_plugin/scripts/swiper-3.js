935fb7d509a144ac4f187a08d075eec2d8fe49d5
ryuhyoung92-rgb <ryuhyoung92@gmail.com>
2026-03-24 12:54:42 +0900

    swiper-button-prev,next

---
+++ swiper_plugin/scripts/swiper-3.js
@@ -0,0 +1,26 @@
+// swiper-3.js
+const ssf = document.querySelector('.ssf_hero');
+const nike = document.querySelector('.brand_slide');
+console.log(ssf, nike);
+
+const ssfSwiper = new Swiper(ssf,{
+    slidesPerView:3, //한번에 보이는 슬라이드 개수
+    spaceBetween:0, //슬라이드 사이 여백
+    loop:true,
+    autoplay:{delay:2000,},
+    // pagination:{el:'',},
+    navigation:{
+        prevEl:'.ssf_hero .prev',
+        nextEl:'.ssf_hero .next',
+    },
+})
+
+const brandSwiper = new Swiper(nike,{
+    slidesPerView:3,
+    spaceBetween:10,
+    loop:true,
+    navigation:{
+        prevEl:'.right_wrap .prev',
+        nextEl:'.right_wrap .next',
+    },
+})
